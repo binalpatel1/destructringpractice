839d1457826c981909fb027773cfded815fbd96c
binal <69486543+binalpatel1@users.noreply.github.com>
2021-08-19 18:47:25 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
+
+const [honda, tesla] = cars;
+//console.log(honda);
+const {
+  speedStats: { topSpeed: hondaTopSpeed }
+} = honda;
+//console.log(hondaTopSpeed);
+const {
+  speedStats: { topSpeed: teslaTopSpeed }
+} = tesla;
+//console.log(teslaTopSpeed);
+const {
+  coloursByPopularity: [hondaTopColour]
+} = honda;
+const {
+  coloursByPopularity: [teslaTopColour]
+} = tesla;
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
